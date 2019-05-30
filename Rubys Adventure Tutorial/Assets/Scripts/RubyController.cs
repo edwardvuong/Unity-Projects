ec16574e0aca9ed81999e4dc980151046141b786
Edward Vuong <ed@edwardvuong.com>
2019-05-30 16:02:34 -0700

    Health Collectibles & Damange Zone

--- Rubys Adventure Tutorial/Assets/Scripts/RubyController.cs
+++ Rubys Adventure Tutorial/Assets/Scripts/RubyController.cs
@@ -7,20 +7,21 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2.0f;
+
     public int health { get { return currentHealth; } }
     int currentHealth;
-    bool isInvincible;
-    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
+
         rigidbody2d = GetComponent<Rigidbody2D>();
 
         currentHealth = maxHealth;
+
+        currentHealth = 1;
     }
 
     // Update is called once per frame
@@ -34,28 +35,12 @@ public class RubyController : MonoBehaviour
         position.y = position.y + speed * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
-
-        if (isInvincible)
-        {
-            invincibleTimer -= Time.deltaTime;
-            if (invincibleTimer < 0)
-                isInvincible = false;
-        }
     }
 
     public void ChangeHealth(int amount)
     {
-        if (amount < 0)
-        {
-            if (isInvincible)
-                return;
-
-            isInvincible = true;
-            invincibleTimer = timeInvincible;
-        }
-
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-
-        Debug.Log(currentHealth + "/" + maxHealth);
+        Debug.Log(currentHealth + "/" +maxHealth);
     }
+
 }
